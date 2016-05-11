142be0f84adc5bf09608b61950161d9c15ba63e9
Ari <t-arima@atgs.jp>
2016-05-11 17:05:31 +0900

    HomeControllerのコメントを修正
    HomeControllerのコメントを修正しました。

--- library/src/main/java/com/manage/library/controller/HomeController.java
+++ library/src/main/java/com/manage/library/controller/HomeController.java
@@ -1,11 +1,9 @@
 /**
- *  パッケージ名。
+ *  コントローラクラスのパッケージ。
  */
 package com.manage.library.controller;
 
-/**
- * 必要なライブラリをインポート。
- */
+// 必要なライブラリをインポートする。
 import java.util.List;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,44 +11,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.manage.library.BooksJoinLendingsAndUsers;
 import com.manage.library.dao.BooksJoinLendingsAndUsersDao;
 
 /**
- * 貸出図書一覧画面に対応するコントローラクラス。
+ * 貸出図書一覧画面のコントローラクラス。
  */
 @Controller
 public class HomeController {
 
-  /**
-   * 値を注入する為のオブジェクトを宣言する。
-   * 
-   * @param BooksJoinLendingsAndUsersDaoView
-   *          結合したテーブルから取得した値を注入するオブジェクト
-   */
   @Autowired
-  private BooksJoinLendingsAndUsersDao booksJoinLendingsAndUsersDaoView;
+  private BooksJoinLendingsAndUsersDao booksJoinLendingsAndUsersDaoView; // dbからbooks(本)、lendings(貸出履歴)、users(ユーザ)テーブルの、それぞれのデータを取得する為のオブジェクト。
 
   /**
-   * 貸出図書一覧画面に遷移するメソッド。 ホーム画面へのリクエストが来た時に実行される。
+   * 貸出図書一覧画面を表示するメソッド。 ホーム画面へのGETリクエストが来た時に実行される。
    * 
-   * @return bookDetail
+   * @return home
    */
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String home(Locale locale, Model model) {
 
-    /**
-     * dbから取得したリストをビューに返す処理。
-     * BooksJoinLendingsAndUsersDaoインターフェースのfindAllメソッドを使い、テーブルから対応するデータを全て取得し、
-     * 取得したデータを、booksという名前でModel型オブジェクトに設定する。
-     */
+    // 一覧画面の表示に必要なデータを得る為、dbのbooks、lendings、usersテーブルからデータを全て取得する。
     List<BooksJoinLendingsAndUsers> homeList = booksJoinLendingsAndUsersDaoView.findAll();
+    // 取得したデータを画面に表示する為、booksという名前でviewに渡す。
     model.addAttribute("books", homeList);
 
-    /**
-     * viewファイルを返す。
-     */
+    // viewファイルが返され、home.jspが表示される。
     return "home";
   }
 }
